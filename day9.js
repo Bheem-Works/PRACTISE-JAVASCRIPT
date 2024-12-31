8cdf3f8163d1cb0825d40ad31df79802c8100401
Bhim Magar <shokokomichan098@gmail.com>
2025-01-01 00:32:09 +0545

    first commit

---
+++ day9.js
@@ -0,0 +1,87 @@
+// // loops in objectes
+// // let miso = ['cat','dog','vari'];
+// // for(let i = 0; i<miso.length;i++) {
+// //     const value = miso[i];
+// //     console.log(value);
+// // }
+
+// // rock paper scissor game night time
+// // using for while loops 
+// // let r = 1/2;
+// // let p = 2/2;
+// // let s = 3/2;
+ 
+// //     if(r<=0.5) {
+// //     console.log("you have chossen the rock ");
+// // } else if (p<=1) {
+// //     console.log('you have chossen the paper');
+// // } else if (s<=1.5) {
+// //     console.log('You have choosen the scissors');
+// // } else {
+// //     console.log('You have to enter the numbers');
+// // }
+
+
+// // // comparing the largest maximum numbers
+// // let a = 10 , b = 20 , c = 15;
+// // if(a>b&&a>c) {
+// //     console.log('The largest number is '+a);
+// // } else if (b>a&&b>c) {
+// //     console.log('The large number is: '+ b);
+// // } else {
+// //     console.log('The largest numbers is c');
+// // }
+
+
+// // simple login 
+
+// // let username = 'miso';
+// // let passwords = 12345;
+// // if(username==='miso'&&passwords===123) {
+// //     console.log('YOu are succesfully login');
+// // } else {
+// //     console.log('YOu are invalid/ YOu can not login');
+// // }
+
+// // simple calculators 
+// let num1 = 5;
+// let num2 = 5;
+// let operators = "-";
+// if(operators === "+") {
+//     console.log("result:"+(num1+num2));
+// } else if(operators === "-") {
+//     console.log("results:"+(num1-num2));
+// } else if(operators === "*") {
+//     console.log("results:"+(num1*num2));
+// } else if(operators === "%") {
+//     console.log("results:"+(num1%num2));
+// } else {
+//     console.log('please enter the value');
+// }
+
+// // create a vowel 
+
+// let char = "e";
+// if(char==='a' || char === 'e' || char ==='i' || char ==='o' || char ==='u') {
+//     console.log(char+': this is the vowel');
+// } else{
+//     console.log(char+'this is consonant');
+// }
+
+// // compare 
+// phrasefloat:The parseFloat function in JavaScript is used to convert a string into a floating-point number (a number with decimals).
+// prompt : prompt is a dailouge box
+// let x = parseFloat(prompt("Enter the first numbers"));
+// let y = parseFloat(prompt('Enter the second Numbers'));
+// if(x>y) {
+//     alert(`${x} is a greater than ${y}`);
+// } else if(y>x) {
+//     alert(`${y} is greater than ${x}`);
+// } else {
+//     alert(`${x} is equal to ${y}`);
+// }
+
+
+// // prompt
+// let miso = prompt('my name is vim');
+// console.log(`miso ${x}`);
