53de3d2a5e9838a26b792bb888f32a752c1cc17d
Bhim Magar <shokokomichan098@gmail.com>
2025-01-01 21:15:32 +0545

    first commit

---
+++ day10.js
@@ -0,0 +1,52 @@
+// function addto() {
+//     let inputelement = document.getElementById('input');
+//     let inputname = inputelement.value;
+//     console.log('Your name:',inputname);
+//     alert(`the string is ${inputname}`);
+// }
+
+// let username = prompt('Enter the username');
+// let admin = prompt('Enter the password');
+
+// if(username==='miso'&& admin ==='124') {
+//     console.log("you are login succefully ");
+//     alert('Yo are login succefully ');
+// } else {
+//     console.log("You cant login");
+//     alert("You can't login bruh");
+// }
+
+// let number1 = prompt('Enter the first number ');
+// let number2 = prompt('Enter the second number ');
+
+// number1 = Number(number1);
+// number2 = Number(number2);
+
+// if(number1>number2) {
+//     console.log("The Number1 is greater than Number2");
+//         alert(`the ${number1} is greater than ${number2}`);
+// } else if(number2>number1) {
+//     console.log("The Number2 is greater than Number1");
+//     alert(`the ${number2} is greater than ${number1}`);
+// } else {
+//     console.log("Both numbers are equal ");
+//     alert('Both Numbers are equal');
+// }
+
+// NOte: donot forget to use if statement
+
+let number1 = prompt("ENter the first numbers");
+let number2 = prompt("ENter the first numbers");
+
+number1 = Number(number1);
+number2 = Number(number2);
+
+if (number1 > number2) {
+  console.log("The Number1 is greater than than the numbers2");
+  alert(`${number1}Is greater than the ${number2}`);
+} else if (number2 > number1) {
+  console.log("The numbers 2 is greater than than the number2");
+  alert(`${number2} Is a greater than the ${number1}`);
+} else {
+  console.log("The numbers value is equal");
+}
