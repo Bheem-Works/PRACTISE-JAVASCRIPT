a811f8c31b99177203b2503e23a2705b06002ffd
Bhim Magar <shokokomichan098@gmail.com>
2025-01-05 18:22:48 +0545

    first commit

---
+++ function1.js
@@ -0,0 +1,73 @@
+// function add(a,b) {
+//     return a + b;
+//  }
+// const numbers = add(2,4);
+// console.log(numbers);
+
+// function multi(x){
+//     return x * 2;
+// }
+// const result = multi(2)+ multi(2);
+// console.log(result);
+
+// function miso(x) {
+//     return x*x;
+// }
+//  console.log(miso(2));
+//  console.log(miso(4));
+
+// function miso(a) {
+//     let b = 9;
+//     if (b > a) {
+//       return `${b} is greater than &{a}`;
+//     } else {
+//       return `${a} is smaller than ${b}`;
+//     }
+//   }
+//   console.log(miso(19));
+//   console.log(miso(10));
+
+//   // math radius functions
+//   function math(radius) {
+//     return Math.PI * radius * radius;
+//   }
+//   const area = math(2);
+//   console.log(area);
+
+// using place holders
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
+// greet('vim');
+
+//   input by using
+
+// functions by using eventlistner
+
+// let c;
+// function a() {
+//   let a = 3;
+//   let b = 5;
+//   c = a + b;
+//   console.log(c);
+// }
+// a();
+
+
+// function redmi(a,b) {
+//   return 2 + 4;
+// }
+// console.log(redmi());
+
+// function name(p1,p2) {
+//   return p1 + p2;
+// } 
+// console.log(name(2,4));
+
+
+// by using the varaible 
+let charlie = "charle is a good boy";
+function pets() {
+  console.log(charlie);
+}
+pets();
