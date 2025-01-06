f26dca0a1b3c376a1ff178214eab846cd025a6ea
Bhim Magar <shokokomichan098@gmail.com>
2025-01-06 13:37:05 +0545

    first commit

---
+++ dayweek.js
@@ -0,0 +1,74 @@
+//1 . function declare : to declare the functions
+
+function sayHello () {
+    console.log("My cat is so handsome");
+}
+
+// Task 3: Check If a Number Is Even or Odd
+// Write a function called checkEvenOdd that takes one parameter (num).
+// Inside the function, check if the number is even or odd using the modulus operator (%).
+// Print "Even" or "Odd" based on the number.
+// Test the function with numbers like 2, 7, and 10
+
+// function numberCheck(a){
+//     if( a%2==0) {
+//         // console.log(a +" is even");
+//         alert(`${a}Is a even`)
+//     } else{
+//         // console.log(a+" is odd");
+//         alert(`${a} Is a odd`)
+//     }
+// }
+// numberCheck(3);
+// numberCheck(7);
+// numberCheck(2);
+
+// how i did it : so first create a functions and set a argument name nad create if else conditions as you want. 
+// after that creating it you also must have to create a variable if you want to change the value. after that write the functions name and in the functions you can give a variable name 
+
+
+// Task 4: Add a Default Parameter
+// Create a function welcomeUser that takes one parameter name.
+// Set a default value of "Guest" for name.
+// Inside the function, print "Welcome, [name]!".
+// Test the function with and without providing an argument (e.g., welcomeUser() and welcomeUser("Bheem")).
+
+// // solutions
+// function high (a="guest") {
+//     console.log("Welcome"+a);
+// }
+// high();
+// high("miso")
+
+// let n = "nima";
+// function aim(){
+//     console.log("WELCOME  "   +n);
+// }
+// aim();
+// n = "Anji nani ";
+// aim();
+
+
+
+
+
+
+// 2. functions calling 
+// sayHello();
+
+
+
+// // 3.parameter and arguments
+// function greet(name) {
+//     console.log("Hi"+name);
+// }
+// greet("Miso");
+
+
+
+// // Return statement
+// function add (a,b) {
+//     return a+b;
+// }
+// let sum = add(2,3);
+// console.log(sum);
