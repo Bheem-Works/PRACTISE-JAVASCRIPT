d4e9b8878c5239710b006fc1e9a8287de538c412
Bhim Magar <shokokomichan098@gmail.com>
2025-01-08 13:36:56 +0545

    first commit

---
+++ youtube.js
@@ -0,0 +1,44 @@
+// by super simple dev
+
+// function: it means we can reuse as many times as we wants 
+
+// return functions
+
+// function belly(a,b) {
+//     return 2+2;
+// }
+// console.log(belly());
+
+// let pen;
+// function write() {
+//     pen = "this pen has  no ink ";
+//     return pen;
+// }
+// console.log(write());
+
+// function pipe() {
+//     console.log(200*3);
+// }
+// pipe(); 
+
+// // parameter
+// function Chau(Red,Blue) {
+//     console.log(Red*Blue);
+// }
+// Chau(2,3);
+
+// function sky (num) {
+//     return num;
+// }
+// function harl (num) {
+//     return num;
+// }
+
+// function black(sky,harl) {
+//     console.log(sky*harl);
+// }
+
+// black(sky(3),harl(3));
+
+let add = (a,b) => a+b;
+console.log(add(3,4));
