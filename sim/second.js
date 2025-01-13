c47b98839bb934dc7e8582928404c647176ca470
Bhim Magar <shokokomichan098@gmail.com>
2025-01-13 15:50:12 +0545

    first commit

---
+++ sim/second.js
@@ -0,0 +1,66 @@
+// changing varaible
+let name = "kim miso";
+name = "Miro muso";
+console.log(name);
+
+// reverse one 
+let perman = 'charge';
+console.log(perman.split('').reverse().join(''));
+
+let value = [1,2,3,4,5];
+let value2 = value.map(valuep=>valuep+2);
+console.log(value2);
+
+// retun with by using symbols
+ var jasu = broke => `she has broken miso heart at many pieces${broke}`;
+ console.log(jasu(''));
+
+// simple functions
+let vio = () => 'my name is vim';
+console.log(vio());
+
+
+
+let person = ["ds","ewr","ertr"]
+
+person.map((i,index)=>{
+if(i=="ds"){
+    console.log("ds")
+}
+})
+
+
+let ewew = ['vim','name','koom'];
+ewew.map((w)=>{
+     if(w=='vim') {
+        console.log('this is mathces') 
+        
+     } else{
+        console.log('thsi conditions is not matched');
+     }
+})
+
+
+//like here is the example of the mapping  
+let kimbey = ['miro','soMo','man'];
+kimbey.map((s)=>{
+    if(s=='miro') {
+        console.log('right');
+    } else{
+        console.log('incorrect');
+    }
+})   
+
+
+const number = [1,2,3,4]
+const result = number.map(num => num+1);
+console.log(result);
+
+
+const jio = [3,4,5] 
+const remo = jio.map(num=>num+1) 
+console.log(remo);
+
+
+let a = () => 'this is my name';
+console.log(a());
